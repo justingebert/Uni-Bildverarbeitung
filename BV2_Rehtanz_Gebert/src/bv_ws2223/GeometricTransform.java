0c8f76389d22ec600db41b62cc248f3d26cbcb80
Justin <justin.gebert@student.htw-berlin.de>
2022-11-03 13:43:01 +0100

    Bilinear neu - noch nicht fertig

--- BV2_Rehtanz_Gebert/src/bv_ws2223/GeometricTransform.java
+++ BV2_Rehtanz_Gebert/src/bv_ws2223/GeometricTransform.java
@@ -135,12 +135,101 @@ public class GeometricTransform {
 			}
 		}
 
+		for(int yDst = 0;yDst<dst.height;yDst++){
+			for(int xDst = 0;xDst<dst.width;xDst++){
 
-		int dstW = (int) ((src.width/2) * (s * sin * (src.width/2) + 1));
+				int width = dst.width;
+				int height = dst.height;
+
+				int xDstC = xDst - (dst.width / 2);
+				int yDstC = yDst - (dst.height / 2);
+
+				double ySrcC = (yDstC / (cos - (yDstC * s * sin)));
+				double xSrcC = (xDstC * (s * sin * ySrcC + 1));
+
+				double xSrc = (xSrcC + (src.width / 2));
+				int xSrcCeil = (int) Math.ceil(xSrcC + (src.width / 2));
+				int xSrcFloor = (int) Math.floor(xSrcC + (src.width / 2));
+				int xSrcRound = (int) Math.round(xSrcC + (src.width / 2));
+
+
+				double ySrc = (ySrcC + (src.height / 2));
+				int ySrcCeil = (int) Math.ceil(ySrcC + (src.height / 2));
+				int ySrcFloor = (int) Math.floor(ySrcC + (src.height / 2));
+				int ySrcRound = (int) Math.round(ySrcC + (src.height / 2));
+
+				double v1 = ySrcCeil - ySrc;
+				double v2 = ySrc - ySrcFloor;
+				double h1 = xSrcCeil - xSrc;
+				double h2 = xSrc - xSrcFloor;
+
+				double v;
+				double h;
+
+				v = Math.min(v1, v2);
+
+				h = Math.min(h1, h2);
+
+
+
+				int rgbA;
+				int rgbB;
+				int rgbC;
+				int rgbD;
+				if(ySrcRound >= src.height-1 || xSrcRound >= src.width-1 || ySrcRound<0 || xSrcRound<0){
+					rgbA = 0xFFFFFFFF;
+					rgbB = 0xFFFFFFFF;
+					rgbC = 0xFFFFFFFF;
+					rgbD = 0xFFFFFFFF;
+				}else{
+					rgbA = src.argb[pos(xSrcRound,ySrcRound,src.width)];
+					rgbB = src.argb[pos(xSrcRound+1,ySrcRound,src.width)];
+					rgbC = src.argb[pos(xSrcRound,ySrcRound+1,src.width)];
+					rgbD = src.argb[pos(xSrcRound+1,ySrcRound+1,src.width)];
+				}
+
+
+
+				int rA = (rgbA >> 16) & 0xff;
+				int gA = (rgbA >> 8) & 0xff;
+				int bA = rgbA & 0xff;
+				int rB = (rgbB >> 16) & 0xff;
+				int gB = (rgbB >> 8) & 0xff;
+				int bB = rgbB & 0xff;
+				int rC = (rgbC >> 16) & 0xff;
+				int gC = (rgbC >> 8) & 0xff;
+				int bC = rgbC & 0xff;
+				int rD = (rgbD >> 16) & 0xff;
+				int gD = (rgbD >> 8) & 0xff;
+				int bD = rgbD & 0xff;
+
+				// Formel von den Folien
+				int rNew = (int) Math.round(rA * (1 - h) * (1 - v) + rB * h * (1 - v) + rC * (1 - h) * v + rD * h * v);
+				int gNew = (int) Math.round(gA * (1 - h) * (1 - v) + gB * h * (1 - v) + gC * (1 - h) * v + gD * h * v);
+				int bNew = (int) Math.round(bA * (1 - h) * (1 - v) + bB * h * (1 - v) + bC * (1 - h) * v + bD * h * v);
+
+
+				int posDst = yDst * dst.width + xDst;
+				dst.argb[posDst] = (0xff << 24) | (rNew << 16) | (gNew << 8) | (bNew);
+			}
+		}
+
+
+
+		/*int dstW = (int) ((src.width/2) * (s * sin * (src.width/2) + 1));
 		int dstH = (int) ((src.height) / (cos - ((src.height/2) * s * sin)));
 
 			for(int yDst = 0;yDst<dst.height;yDst++){
 				for(int xDst = 0;xDst<dst.width;xDst++){
+
+					int xDstC = xDst - (dst.width / 2);
+					int yDstC = yDst - (dst.height / 2);
+
+					int ySrcC = (int) (yDstC / (cos - (yDstC * s * sin)));
+					int xSrcC = (int) (xDstC * (s * sin * ySrcC + 1));
+
+					int xSrc = xSrcC + (src.width / 2);
+					int ySrc = ySrcC + (src.height / 2);
 
 					int rgbA = 0;
 					int rgbB = 0;
@@ -154,8 +243,8 @@ public class GeometricTransform {
 					int altX = (int) Math.round(ratioW * xDst);
 					int altY = (int) Math.round(ratioH * yDst);
 
-					double h = ratioW * xDst - altX;
-					double v = ratioH * yDst - altY;
+					double h = ratioW * xSrc - altX;
+					double v = ratioH * ySrc - altY;
 
 					rgbA = src.argb[pos(altX, altY, src.width)];
 					if (altX == src.width - 1) {
@@ -231,7 +320,7 @@ public class GeometricTransform {
 				}
 
 
-				}
+				}*/
 			}
 
 
